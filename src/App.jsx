b71c6a8d0dfb71c823766914c117afe0bb2ad81c
DIEGO FERREIRA DE ARAUJO <DIEGO6040@DIGITALCOLLEGE.LOCAL>
2024-08-21 08:49:41 -0300

    feat: adicionando axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function App() {
+  const [dados, setDados] = useState();
+
   useEffect(() => {
     console.log('chamei o useEffect')
 
+    // Maneira 2 - async/await com uma função ASYNC
     const realizarRequest = async () => {
       try {
-        const res = await axios('https://jsonplaceholder.typicode.com/todos/1')
-        console.log(res);
-      } catch (error) {
-        console.log(error);
+        const res = await axios('https://api.discogs.com/releases/23949476?&key=fDPuzDCZvRVudVyXTyMJ&secret=ncLniYJXSgMCoydevODixTIDrgULdzLM')
+        console.log('axios res.data', res.data)
+        setDados(res.data)
+      } catch (erro) {
+        console.log('aconteceu um erro:', erro)
       }
     }
 
     realizarRequest()
+
+    // Maneira ERRADA - await sem async
+    // const res1 = await fetch('https://api.discogs.com/releases/23949476')
+    // const res2 = await res1.json()
+    // console.log(res2)
+
   }, [])
 
+  if (!dados) {
+    return <div>Fazendo o download..</div>
+  }
+
   return (
-    <>
-    <h1>Teste</h1>
-    <div>imagem</div>
-    <div style={{ color: 'red' }}>titulo</div>
-    <div style={{ color: 'blue' }}>artista</div>
+    // <>
+    // <h1>Teste</h1>
+    // <div>imagem</div>
+    // <div style={{ color: 'red' }}>titulo</div>
+    // <div style={{ color: 'blue' }}>artista</div>
+
+    // <div>
+    //   <div>tag 1</div>
+    //   <div>tag 2</div>
+    // </div>
+    // </>
 
     <div>
-      <div>tag 1</div>
-      <div>tag 2</div>
-    </div>
-    </>
+    <a href={dados.uri}>
+      <img src={dados.thumb} />
+    </a>
+    <div>{dados.artists_sort}</div>
+    <div>{dados.title}</div>
+    <div>{dados.styles.map(item => <div>{item}</div>)}</div>
+  </div>
   )
 }
 
